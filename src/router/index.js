e526a676a74d506d50ceee5c2687620387dbcff9
Lappand233 <2494907382@qq.com>
2021-11-29 17:14:07 +0800

    Routing optimization

--- src/router/index.js
+++ src/router/index.js
@@ -2,36 +2,48 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/Login'
 import Home from '@/views/Home'
+import Error404 from '@/views/error/404'
 
 import Global from '@/Global'
 
 Vue.use(VueRouter)
 
-const index = Global.isEmpty(Global.token) ? '/login' : '/home'
-const routes = [
-    {
-        path: '/',
-        redirect: index
-    },
-    {
-        path: '/login',
-        component: Login,
-        meta: {
-            title: '登录'
-        }
-    },
-    {
-        path: '/home',
-        component: Home,
-        meta: {
-            title: '首页'
-        }
+// const redirect = (path, redirect) => ({
+//     path,
+//     redirect
+// })
+// const route = (path, component, title) => ({
+//     path,
+//     component,
+//     meta: {
+//         title
+//     }
+// })
+const alias = (from, to, component, title) => ({
+    path: to,
+    component,
+    alias: from,
+    meta: {
+        title
     }
+})
+
+const index = Global.isEmpty(Global.token) ? {
+    url: '/login',
+    component: Login,
+    title: '登录'
+} : {
+    url: '/home',
+    component: Home,
+    title: '首页'
+}
+const routes = [
+    alias('/', index.url, index.component, index.title)
 ]
 
-const router = new VueRouter({
-    routes
-})
+routes.push(alias('*', '/404', Error404, '404'))
+
+const router = new VueRouter({ routes })
 
 router.beforeEach((to, from, next) => {
     const { title } = to.meta
---
+++ src/views/error/404.vue
@@ -0,0 +1,18 @@
+<template>
+    <Container>
+        404
+    </Container>
+</template>
+
+<script>
+import Container from '@/components/Container'
+
+export default {
+    name: '404',
+    components: { Container }
+}
+</script>
+
+<style scoped>
+
+</style>
